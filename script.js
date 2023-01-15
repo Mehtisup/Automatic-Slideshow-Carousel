9ac8d640825b3fb2535b223176b07fa1b936098c
Mahdi Bozorgi <102772767+Mehtisup@users.noreply.github.com>
2023-01-15 23:44:25 +0330

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+<script>
+var index = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var items = document.getElementsByClassName("item");
+    for (i = 0 ; i < items.length ; i++) {
+        items[i].style.display = "none";
+    }
+    index++;
+    if (items.length < index) {index = 1}
+    items[index-1].style.display = "block";
+    setTimeout(showSlides, 2000);
+}
+</script>
